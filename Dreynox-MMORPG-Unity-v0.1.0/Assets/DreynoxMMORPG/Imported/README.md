9bad0fab81299ae654f0cbb95fe71cf336b4acbe
Dreynox <admin@dreynox.com>
2026-09-23 01:55:10 -0500

    Delete Dreynox-MMORPG-Unity-v0.1.0 directory

--- Dreynox-MMORPG-Unity-v0.1.0/Assets/DreynoxMMORPG/Imported/README.md
+++
@@ -1,3 +0,0 @@
-# Imported
-
-Salida regenerable de la Fase 1. Los importadores convierten recursos heredados a assets nativos de Unity dentro de esta carpeta.
--- Dreynox-MMORPG-Unity-v0.1.0/Assets/DreynoxMMORPG/Runtime/Gameplay/Equipment/EquipmentSlot.cs
+++
@@ -1,7 +0,0 @@
-namespace Dreynox.Mmorpg.Gameplay.Equipment
-{
-    public enum EquipmentSlot
-    {
-        Head, Torso, Hands, Legs, Feet, MainHand, OffHand, Wings, Mount, Accessory
-    }
-}
