5cc588582ecf4369666854bbaef9be4f8ed33b55
Dreynox <admin@dreynox.com>
2026-09-23 06:45:10 -0500

    fix(vfx): reference MON descriptor namespace in EFT importer

--- Assets/DreynoxMMORPG/Editor/LegacyFormats/LegacyEftPrefabImporter.cs
+++ Assets/DreynoxMMORPG/Editor/LegacyFormats/LegacyEftPrefabImporter.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Dreynox.Mmorpg.Editor.Corpus;
 using Dreynox.Mmorpg.Vfx;
+using Dreynox.Mmorpg.World;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
