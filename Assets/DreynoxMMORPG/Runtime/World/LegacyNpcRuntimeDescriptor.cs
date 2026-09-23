bde6da8b62857c0e439ead6d57b2aef35beab758
Dreynox <admin@dreynox.com>
2026-09-23 06:04:21 -0500

    feat(npc): stream canonical resolved NPCs around the player

---
+++ Assets/DreynoxMMORPG/Runtime/World/LegacyNpcRuntimeDescriptor.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+namespace Dreynox.Mmorpg.World
+{
+    [Serializable]
+    public struct LegacyNpcGateTargetRuntime
+    {
+        public int mapId;
+        public Vector3 position;
+        public int cost;
+    }
+
+    public sealed class LegacyNpcRuntimeDescriptor : MonoBehaviour
+    {
+        [SerializeField] private int npcType;
+        [SerializeField] private int typeId;
+        [SerializeField] private int modelIndex;
+        [SerializeField] private int faction;
+        [SerializeField] private int moveDistance;
+        [SerializeField] private int moveSpeed;
+        [SerializeField] private int merchantType = -1;
+        [SerializeField] private LegacyNpcGateTargetRuntime[] gateTargets =
+            Array.Empty<LegacyNpcGateTargetRuntime>();
+
+        public int NpcType => npcType;
+        public int TypeId => typeId;
+        public int ModelIndex => modelIndex;
+        public int Faction => faction;
+        public int MoveDistance => moveDistance;
+        public int MoveSpeed => moveSpeed;
+        public int MerchantType => merchantType;
+        public IReadOnlyList<LegacyNpcGateTargetRuntime> GateTargets =>
+            gateTargets;
+
+        public void Configure(
+            int type,
+            int id,
+            int model,
+            int npcFaction,
+            int movementDistance,
+            int movementSpeed,
+            int merchant,
+            LegacyNpcGateTargetRuntime[] targets)
+        {
+            npcType = type;
+            typeId = id;
+            modelIndex = model;
+            faction = npcFaction;
+            moveDistance = movementDistance;
+            moveSpeed = movementSpeed;
+            merchantType = merchant;
+            gateTargets =
+                targets ?? Array.Empty<LegacyNpcGateTargetRuntime>();
+        }
+    }
+}
